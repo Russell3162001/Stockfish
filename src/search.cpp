0be08738142139f6a0811817b2dd3166960ca9b6
GuardianRM <strimr25@mail.ru>
2017-08-26 13:53:22 +0300

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -430,6 +430,8 @@ void Thread::search() {
                   && (bestValue <= alpha || bestValue >= beta)
                   && Time.elapsed() > 3000)
                   sync_cout << UCI::pv(rootPos, rootDepth, alpha, beta) << sync_endl;
+                  
+                  delta += delta / 4 + 5;
 
               // In case of failing low/high increase aspiration window and
               // re-search, otherwise exit the loop.
@@ -448,8 +450,6 @@ void Thread::search() {
                   beta = std::min(bestValue + delta, VALUE_INFINITE);
               else
                   break;
-
-              delta += delta / 4 + 5;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }
@@ -1018,7 +1018,7 @@ moves_loop: // When in check search starts from here
       // For PV nodes only, do a full PV search on the first move or after a fail
       // high (in the latter case search only if value < beta), otherwise let the
       // parent node fail low with value <= alpha and try another move.
-      if (PvNode && (moveCount == 1 || (value > alpha && (rootNode || value < beta))))
+      if (PvNode && (moveCount == 1 || (value > alpha)))
       {
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
