05f0c15d64fae62bc283c7a873d93e5da51be880
GuardianRM <strimr25@mail.ru>
2017-08-26 13:51:54 +0300

    Update pawns.cpp

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -3,17 +3,14 @@
   Copyright (C) 2004-2008 Tord Romstad (Glaurung author)
   Copyright (C) 2008-2015 Marco Costalba, Joona Kiiski, Tord Romstad
   Copyright (C) 2015-2017 Marco Costalba, Joona Kiiski, Gary Linscott, Tord Romstad
-
   Stockfish is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-
   Stockfish is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -169,20 +166,24 @@ namespace {
         }
 
         // Score this pawn
+         if (lever)
+            score += Lever[relative_rank(Us, s)];
+        
         if (supported | phalanx)
             score += Connected[opposed][!!phalanx][popcount(supported)][relative_rank(Us, s)];
 
-        else if (!neighbours)
+        else
+       
+        {
+        if (!neighbours)
             score -= Isolated[opposed];
 
-        else if (backward)
+        if (backward)
             score -= Backward[opposed];
 
-        if (doubled && !supported)
+        if (doubled)
             score -= Doubled;
-
-        if (lever)
-            score += Lever[relative_rank(Us, s)];
+         }
     }
 
     return score;
