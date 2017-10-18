c560cc9d6db231b8a332a9302791b9e54bac8fb8
IIvec <ivan.ivec@gmail.com>
2017-10-18 18:35:43 +0200

    Revert "Reformat time manager code"
    This reverts commit e10255339fc7cb54bb0466945f759646f442f4f0.

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -31,15 +31,17 @@ namespace {
   enum TimeType { OptimumTime, MaxTime };
 
   int remaining(int myTime, int myInc, int moveOverhead, int movesToGo,
-                int moveNum, bool ponder, TimeType type) {
+                int ply, bool ponder, TimeType type) {
 
     if (myTime <= 0)
         return 0;
 
-    double ratio; // Which ratio of myTime we are going to use
+    int moveNumber = (ply + 1) / 2;
+    double ratio; // Which ratio of myTime we are going to use. It's <= 1 if not ponder
+    double sd = 8.5;
 
     // Usage of increment follows quadratic distribution with the maximum at move 25
-    double inc = myInc * std::max(55.0, 120 - 0.12 * (moveNum - 25) * (moveNum - 25));
+    double inc = myInc * std::max(55.0, 120.0 - 0.12 * (moveNumber - 25) * (moveNumber - 25));
 
     // In moves-to-go we distribute time according to a quadratic function with
     // the maximum around move 20 for 40 moves in y time case.
@@ -47,26 +49,28 @@ namespace {
     {
         ratio = (type == OptimumTime ? 1.0 : 6.0) / std::min(50, movesToGo);
 
-        if (moveNum <= 40)
-            ratio *= 1.1 - 0.001 * (moveNum - 20) * (moveNum - 20);
+        if (moveNumber <= 40)
+            ratio *= 1.1 - 0.001 * (moveNumber - 20) * (moveNumber - 20);
         else
             ratio *= 1.5;
-
-        ratio *= 1 + inc / (myTime * 8.5);
     }
     // Otherwise we increase usage of remaining time as the game goes on
     else
     {
-        double k = 1 + 20 * moveNum / (500.0 + moveNum);
-        ratio = (type == OptimumTime ? 0.017 : 0.07) * (k + inc / myTime);
+        sd = 1 + 20 * moveNumber / (500.0 + moveNumber);
+        ratio = (type == OptimumTime ? 0.017 : 0.07) * sd;
     }
 
-    int time = int(std::min(1.0, ratio) * std::max(0, myTime - moveOverhead));
+    ratio = std::min(1.0, ratio * (1 + inc / (myTime * sd)));
 
-    if (type == OptimumTime && ponder)
-        time *= 1.25;
+    assert(ratio <= 1);
 
-    if (type == MaxTime)
+    if (ponder && type == OptimumTime)
+        ratio *= 1.25;
+
+    int time = int(ratio * (myTime - moveOverhead));
+
+    if (type == OptimumTime)
         time -= 10; // Keep always at least 10 millisecs on the clock
 
     return std::max(0, time);
@@ -105,11 +109,9 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply)
       limits.npmsec = npmsec;
   }
 
-  int moveNum = (ply + 1) / 2;
-
   startTime = limits.startTime;
   optimumTime = remaining(limits.time[us], limits.inc[us], moveOverhead,
-                          limits.movestogo, moveNum, ponder, OptimumTime);
+                          limits.movestogo, ply, ponder, OptimumTime);
   maximumTime = remaining(limits.time[us], limits.inc[us], moveOverhead,
-                          limits.movestogo, moveNum, ponder, MaxTime);
+                          limits.movestogo, ply, ponder, MaxTime);
 }
